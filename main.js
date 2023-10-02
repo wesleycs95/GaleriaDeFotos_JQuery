a462b123d12e933084ad3de544f0a9f9e319a532
wesleycs95 <wesleycs95@gmail.com>
2023-10-02 14:47:19 -0300

    Meu primeiro projeto com JQuery

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botaoCancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+            const enderecoNovaImagem = $('#enderecoNovaImagem').val();
+            const novoItem = $('<li style= "display:none"></li>');
+            $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+
+            $(`
+            
+            <div class="overleyImagemLink">
+                <a href="${enderecoNovaImagem}" title="Ver imagem em tamanho real" target="_blank">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+
+            `).appendTo(novoItem);
+
+            $(novoItem).appendTo('ul');
+            $(novoItem).fadeIn();
+            $('#enderecoNovaImagem').val('')
+    })
+})
